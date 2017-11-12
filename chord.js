5cb3b751a359598b9033476031aa4f35f2cde0f4
Unknown <marketing.KevinConti@gmail.com>
2017-11-12 10:29:53 -0500

    Add npm prompt as a requirement for this project

--- chord.js
+++ chord.js
@@ -15,4 +15,21 @@ CHORD.userArgs = function(B,N){
     };
 };
 
+var userInput = function getUserInput() {
+    let prompt = require('prompt');
+    prompt.start();
+
+    //
+    // Get two properties from the user: username and email
+    //
+    prompt.get(['username', 'email'], function (err, result) {
+        //
+        // Log the results.
+        //
+        console.log('Command-line input received:');
+        console.log('  username: ' + result.username);
+        console.log('  email: ' + result.email);
+    });
+}();
+
 module.exports = CHORD;
